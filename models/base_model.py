e199c1ac2496ad12eb07ef73148bcd7cc52368d0
Jsalmen13 <jarraya.salmen13@gmail.com>
2021-02-16 23:05:03 +0000

    let's check it

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-321321
+BaseModel
 """
 
 
@@ -9,19 +9,23 @@ from datetime import datetime
 
 
 class BaseModel:
+    """"asba"""
     def __init__(self):
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """"asba"""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__,
                                          self.id, self.__dict__)
 
     def save(self):
+        """"asba"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """"asba"""
         self.__dict__[__class__] = self.__class__.__name__
         self.__dict__["created_at"] = \
             self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
