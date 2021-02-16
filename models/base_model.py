6826a677053ffab271451527c95a68cc091e3767
Jsalmen13 <jarraya.salmen13@gmail.com>
2021-02-16 23:43:42 +0000

    4th task

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,18 @@ from datetime import datetime
 
 class BaseModel:
     """"asba"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+            if (key == "created_at" or key == "updated_at"):
+                if (type(value) is str):
+                        Date_obj = datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f')
+                        self.__dict__[key] = Date_obj
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """"asba"""
